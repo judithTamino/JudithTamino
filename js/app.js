099149c4ea7326fd641c0caf16ab9627e5224e46
יהודית טמינו <taminojudit@hotmail.co.il>
2024-12-04 17:01:07 +0200

    update project

--- js/app.js
+++ js/app.js
@@ -1,17 +1,37 @@
-/*=============== SHOW MENU ===============*/
-const navMenu = document.querySelector('.nav__menu'),
-  navToggle = document.querySelector('.nav__toggle'),
-  navClose = document.querySelector('.nav__close');
+/* ========== MENU SHOW ========== */
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
-// בדיקה אם קיים
-if(navToggle) {
+// show menu
+if (navToggle) {
   navToggle.addEventListener('click', () => {
     navMenu.classList.add('show-menu');
-  })
+  });
 }
 
-if(navClose) {
+//hide menu
+if (navClose) {
   navClose.addEventListener('click', () => {
     navMenu.classList.remove('show-menu');
-  })
+  });
 }
+
+/* ========== REMOVE MENU MOBILE ========== */
+const navLink = document.querySelectorAll(".nav-link");
+
+function linkAction() {
+  // when click on each nav link, we remove the show-menu class
+  navMenu.classList.remove('show-menu');
+}
+navLink.forEach(link => link.addEventListener('click', linkAction));
+
+/* ========== HTML & CSS PROJECTS SWIPER ========== */
+let swiper = new Swiper(".services-container", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
